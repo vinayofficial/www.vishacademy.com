de9917462277ea637ae37ba4c9b3aad5ad23efde
Vinay singh <vinaysinghofficial@gmail.com>
2015-01-04 01:32:31 +0530

    test on bit

--- index.php
+++ index.php
@@ -1,2 +1,314 @@
 <?php include "resources/header.php"; ?>
-<?php include "resources/footer.php"; ?>
+  <div class="jumbotron front">
+    <div class="col-lg-12 col-md-12">
+      <div class="col-lg-6 col-md-6 welcome-text">
+        <h2>Free Computer science educational videos in Hindi language</h2>
+        Start to upgrade your knowledge for free<br />
+        <button class="btn btn-lg btn-primary mt10"> Start to learn </button>
+      </div>
+      <div class="col-lg-6 col-md-6 site-trailer-box">
+        <div class="site-trailer"> </div>
+      </div>
+    </div>
+  </div>
+  <div class="frontuserreview">
+    <div class="col-lg-9 col-centered"> "I have been very much impressed by Repair shopr's ability to literally read our minds and deliver a solution that perfectly gathers everything I was looking for, all in one simple place. Needless to say we are extremely satisfied. Thank you!"						     </div>
+  </div>
+  <!--latest uploads-->
+  <div id="latestupload">
+  	<div class="heading-strip" style="background:#EA464A; color:#fff;">
+  		<h2>// Latest Uploads</h2>
+    </div>
+    <div class="col-lg-12">
+    	<div class="col-lg-4">
+        	<h4 class="exclusive-heading" style="color:#3D9EDC;">// For Beginners</h4>
+            <div class="col-lg-6 latest-box">
+            	<img src="assets/images/layout/Android.png">
+                <h4>Android Development workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+           <div class="col-lg-6 latest-box">
+            	<img src="assets/images/layout/Android.png">
+                <h4>Android Development workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+        </div>        
+    	<div class="col-lg-4">
+        	<h4 class="exclusive-heading" style="color:#2C9787;">// For Stuffers</h4>
+            <div class="col-lg-6 latest-box">
+            	<img src="assets/images/layout/frontendwebdevelopment.png">
+                <h4>Responsive web design workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-6 latest-box">
+            	<img src="assets/images/layout/frontendwebdevelopment.png">
+                <h4>Responsive web design workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+        </div>
+    	<div class="col-lg-4">
+        	<h4 class="exclusive-heading" style="color:#EFC849;">// For Trickers</h4>
+            <div class="col-lg-6 latest-box">
+            	<img src="assets/images/layout/JAVA.png">
+                <h4>Basic of JAVA Programming</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-6 latest-box">
+            	<img src="assets/images/layout/JAVA.png">
+                <h4>Basic of JAVA Programming</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+        </div>
+    </div>
+  </div>
+  <div class="clr"></div>
+  <!--Beginners--->  
+  <section class="course_section">
+  		<div class="level-strip" style="background:#3D9EDC; color:#fff;">
+  		<h2>// Beginners Courses</h2>
+    </div>
+    	<div class="col-lg-12">    	
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Cassandra.png">
+                <h4>learn Cassandra in hindi</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Android.png">
+                <h4>Android Development workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/cprogramming.png">
+                <h4>C programming tutorials</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/frontendwebdevelopment.png">
+                <h4>Responsive web design workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/JAVA.png">
+                <h4>Basic of JAVA Programming</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Python-For-Big-Data-Analytics.png">
+                <h4>Kickstart with Python script</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Cassandra.png">
+                <h4>learn Cassandra in hindi</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Android.png">
+                <h4>Android Development workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/cprogramming.png">
+                <h4>C programming tutorials</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/frontendwebdevelopment.png">
+                <h4>Responsive web design workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/JAVA.png">
+                <h4>Basic of JAVA Programming</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Python-For-Big-Data-Analytics.png">
+                <h4>Kickstart with Python script</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+        </div>
+    </section>    
+    <div class="clr"></div>
+ <!--Stuffers--->  
+ 	<section class="course_section">
+  		<div class="level-strip" style="background:#2C9787; color:#fff;">
+  		<h2>// Stuffers Courses</h2>
+    </div>
+    	<div class="col-lg-12">    	
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Cassandra.png">
+                <h4>learn Cassandra in hindi</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Android.png">
+                <h4>Android Development workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/cprogramming.png">
+                <h4>C programming tutorials</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/frontendwebdevelopment.png">
+                <h4>Responsive web design workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/JAVA.png">
+                <h4>Basic of JAVA Programming</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Python-For-Big-Data-Analytics.png">
+                <h4>Kickstart with Python script</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Cassandra.png">
+                <h4>learn Cassandra in hindi</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Android.png">
+                <h4>Android Development workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/cprogramming.png">
+                <h4>C programming tutorials</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/frontendwebdevelopment.png">
+                <h4>Responsive web design workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/JAVA.png">
+                <h4>Basic of JAVA Programming</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Python-For-Big-Data-Analytics.png">
+                <h4>Kickstart with Python script</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+        </div>
+    </section> 
+  <div class="clr"></div>
+ <!--trickers--->  
+ 	<section class="course_section">
+  		<div class="level-strip" style="background:#EFC849; color:#fff;">
+  		<h2>// Trickers Courses</h2>
+    </div>
+    	<div class="col-lg-12">    	
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Cassandra.png">
+                <h4>learn Cassandra in hindi</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Android.png">
+                <h4>Android Development workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/cprogramming.png">
+                <h4>C programming tutorials</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/frontendwebdevelopment.png">
+                <h4>Responsive web design workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/JAVA.png">
+                <h4>Basic of JAVA Programming</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Python-For-Big-Data-Analytics.png">
+                <h4>Kickstart with Python script</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Cassandra.png">
+                <h4>learn Cassandra in hindi</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Android.png">
+                <h4>Android Development workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/cprogramming.png">
+                <h4>C programming tutorials</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/frontendwebdevelopment.png">
+                <h4>Responsive web design workshop</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/JAVA.png">
+                <h4>Basic of JAVA Programming</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+            <div class="col-lg-2 latest-box">
+            	<img src="assets/images/layout/Python-For-Big-Data-Analytics.png">
+                <h4>Kickstart with Python script</h4>
+                <span class="text-muted pull-left text-small w50p">// web design</span>
+                <span class="text-muted pull-right text-small w50p text-right">// 5 hours ago</span>
+            </div>
+        </div>
+    </section>
+    <div class="clr"></div>
+  <?php include "resources/footer.php"; ?>
